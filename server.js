e714273cf1e16938b27591ca7601e93d2189e5d4
Chaise Matev <aj_fl81@msn.com>
2020-03-02 19:36:16 -0500

    initial commit

--- server.js
+++ server.js
@@ -4,6 +4,12 @@ const db = require('./db');
 
 const app = express();
 
+app.use(express.json());
+app.use((req, res, next) => {
+  console.log(req.method, req.url);
+  next();
+});
+
 app.use('/dist', express.static(path.join(__dirname, 'dist')));
 
 app.get('/', (req, res, next) => {
